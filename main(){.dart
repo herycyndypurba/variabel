d26d3b4fdd78c22dda44200a7b9fbc4c760ff939
rafikaramadhani <78518119+rafikaramadhani@users.noreply.github.com>
2021-02-24 00:44:03 -0800

    Add files via upload

---
+++ main(){.dart
@@ -0,0 +1,15 @@
+    main(){
+    // membaut variabel dengan tipe data
+    String nama = "Rafika";
+    int umur = 21;
+    double tinggi = 155.43;
+    bool isMenikah = false;
+
+    // membuat variabel dengan kata kunci var
+    var alamat = "Medan, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
